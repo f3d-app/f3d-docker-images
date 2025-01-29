ace6745c4d54b8eb0c08a9c8b6a4901101de0edc
Michael Migliore <mcmigliore@gmail.com>
2025-01-29 10:33:30 +0100

    try

--- android/android-toolchain.cmake
+++ android/android-toolchain.cmake
@@ -1,6 +1,8 @@
+# See https://cmake.org/cmake/help/latest/manual/cmake-toolchains.7.html#cross-compiling-for-android-with-the-ndk
 set(CMAKE_SYSTEM_NAME Android)
-set(CMAKE_SYSTEM_VERSION $ENV{ANDROID_API_VERSION})
+set(CMAKE_SYSTEM_VERSION $ENV{ANDROID_API})
+
 set(CMAKE_ANDROID_ARCH_ABI $ENV{ANDROID_ARCH})
 set(CMAKE_ANDROID_NDK /ndk)
 
-set(CMAKE_FIND_ROOT_PATH /depends/$ENV{ANDROID_ARCH})
+set(CMAKE_FIND_ROOT_PATH /depends)
